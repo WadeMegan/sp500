92935cb0e6a1b268b7c3db8d53df47bea581d851
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-07-16 22:54:35 -0500

    form working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react'
 import './App.css'
 import SpChart from '../SpChart/SpChart'
+import Logo from '../../assets/white-logo.png'
 
 export default class App extends Component {
 
   render(){
     return (
-      <SpChart/>
+      <>
+      <nav>
+        <ul>
+          <li><a href="https://portfolio.wademegan.now.sh/" target="_blank" rel="noopener noreferrer" aria-label="portfolio link"><img src={Logo}></img></a></li>
+          <li><a href="https://www.linkedin.com/in/megan-wade-909129124/" target="_blank" rel="noopener noreferrer" aria-label="linkedin link"><i className="fab fa-linkedin-in fa-2x"></i></a></li>
+          <li><a href="https://github.com/WadeMegan" target="_blank" rel="noopener noreferrer" aria-label="github link"><i className="fab fa-github fa-2x"></i></a></li>
+        </ul>
+      </nav>
+      <main>
+        <SpChart/>
+      </main>
+      </>
     )
   }
   
--- src/components/SpChart/SpChart.js
+++ src/components/SpChart/SpChart.js
@@ -1,43 +1,97 @@
 import React, {Component} from 'react'
 import './SpChart.css'
 import SpData from './SpData'
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export default class SpChart extends Component {
+
+    state={
+        startYear:1926,
+        endYear:2019,
+    }
 
   renderData=()=>{
     //sort data in ascending order based on year
     const spDataSorted = SpData.sort((a,b)=>a.year-b.year)
     
     let dataRows = spDataSorted.map((row,index)=>{
-        return(
-            <tr key={index}>
-                <td>{row.year}</td>
-                <td>{row.totalReturn}</td>
-                <td>Test</td>
-            </tr>
-        )
+        if(row.year>=this.state.startYear && row.year<=this.state.endYear){
+
+            let currentYear = row.year
+            let cumulativeReturn = 0
+
+            SpData.map(row=>{
+                if(row.year<=currentYear){
+                    cumulativeReturn = cumulativeReturn+Number(row.totalReturn)
+                }
+            })
+
+            return(
+                <tr key={index} className="hoverableTr">
+                    <td>{row.year}</td>
+                    <td>{row.totalReturn}</td>
+                    <td>{cumulativeReturn.toFixed(2)}</td>
+                </tr>
+            )
+        }
+        
     })
 
     return dataRows
   }
 
+  handleRangeChange=(range)=>{
+      console.log(range)
+      this.setState({
+          startYear:range[0],
+          endYear:range[1]
+      })
+  }
+
+  renderRange=()=>{
+
+    let marks={
+        1926:{
+            style: {
+                display:"block",
+            },
+            label: "1926",
+        },
+        2019:{
+            style:{
+                display:"block",
+            },
+            label:"2019"
+        }
+    }
+
+    return(
+        <Range className='range' min={1926} max={2019} defaultValue={[1926,2019]} onAfterChange={range=>this.handleRangeChange(range)} allowCross={false} marks={marks}/>
+    )
+
+  }
+
   render(){
     return (
-        <>
-        <h1>S&P 500 Index Returns</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Year</th>
-                    <th>Total Return</th>
-                    <th>Cumulative Returns</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.renderData()}
-            </tbody>
-        </table>
-        </>
+        <section className='chartSection'>
+            <header>
+                <h1>S&P 500 Returns by Year</h1>
+            </header>
+            {this.renderRange()}
+            <table>
+                <thead>
+                    <tr className='tr-border'>
+                        <th>Year</th>
+                        <th>Total Return</th>
+                        <th>Cumulative Returns</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderData()}
+                </tbody>
+            </table>
+        </section>
     )
   }
   
